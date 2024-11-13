dcfe5e88240024d91be39c38e4b7e58c1e5b3cb4
webdevyoo <yjk7454@naver.com>
2024-11-13 21:13:46 +0900

    chore: `webpack.config.js` - mode, devtools, extensions 설정 추가

--- 4. 숫자야구/webpack.config.js
+++ 4. 숫자야구/webpack.config.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 /* 모듈 내보내기 - 웹팩킹에 대한 webpack 설정 */
 module.exports = {
+  mode: 'development', // 개발시 development 배포시 production
+  devtool: 'eval', // 개발시 eval(속도가 빠름) 배포시 hidden-source-map
+  resolve: {
+    extensions: ['.js', '.vue'] // 확장자 처리 : import시  .js, .vue 확장자 생략이 가능
+  },
   /**
    * 모든 script를 하나로 합칠때 대표가되는 파일
    * 키(프로퍼티)로 접근한다.
