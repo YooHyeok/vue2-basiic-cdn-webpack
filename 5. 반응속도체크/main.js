8bbf8a92d3a9e892e4a98a8d6ae6a04e1620db20
webdevyoo <yjk7454@naver.com>
2024-11-15 21:15:30 +0900

    feat: `5. 반응속도 체크` 프로젝트 초기 세팅

---
+++ 5. 반응속도체크/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { createApp } from 'vue';
+import ResponseCheck from './ResponseCheck'
+
+// new Vue(ResponseCheck).$mount('#root'); // vue2 마운트
+createApp(ResponseCheck).mount('#root'); // vue3 마운트
