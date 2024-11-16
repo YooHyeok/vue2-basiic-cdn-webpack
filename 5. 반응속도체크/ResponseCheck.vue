27346b03d585df0890b81ba58a8a4d59c895f994
webdevyoo <yjk7454@naver.com>
2024-11-16 18:49:06 +0900

    feat: `5. 반응속도 체크 루트 컴포넌트` - 기본 레이아웃 및 데이터, 메소드 구현

--- 5. 반응속도체크/ResponseCheck.vue
+++ 5. 반응속도체크/ResponseCheck.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
+    <div id="screen">{{  }}</div> <!-- 클릭 영역 -->
+    <div>
+      <div>평균 시간: {{  }}</div>
+      <button @click="onReset">리셋</button>
+    </div>
   </div>
 </template>
 <script>
   export default {
     data() {
       return {
+        result: [],
+        state: 'waiting',
+        message: '클릭해서 시작하세요.',
       }
     },
     methods: {
-      method(e) {
+      onReset() {
       }
     },
   }
