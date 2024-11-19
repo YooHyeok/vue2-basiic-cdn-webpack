0f921c47dd9607dade8b5bc72612218fd7b43fe3
webdevyoo <yjk7454@naver.com>
2024-11-20 00:02:11 +0900

    feat: `6. 가위바위보` - 초기 프로젝트 세팅

---
+++ 6. 가위바위보/main.js
@@ -0,0 +1,6 @@
+// import Vue from 'vue'
+import { createApp } from 'vue';
+import RockScissorsPapper from './RockScissorsPapper'
+
+// new Vue(RockScissorsPapper).$mount('#root'); // vue2 마운트
+createApp(RockScissorsPapper).mount('#root'); // vue3 마운트
