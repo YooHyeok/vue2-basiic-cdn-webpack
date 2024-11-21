0818e0c7e2d5e4db4cd084dde7a1bae80dde99b8
webdevyoo <yjk7454@naver.com>
2024-11-21 21:35:43 +0900

    feat: `7. LottoGenerator.vue` - 로또 번호 생성 함수 구현 및 winNumbers 초기화

--- 7. 로또/LottoGenerator.vue
+++ 7. 로또/LottoGenerator.vue
@@ -11,21 +11,33 @@
 </template>
 <script>
   import LottoBall from './LottoBall'
+  /**
+   * 로또 번호 생성
+   */
+  const getWinNumbers = () => {
+    const candidate = Array(45).fill().map((value, index) => index + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+      shuffle.push(candidate.splice(Math.floor( Math.random() * candidate.length ), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length -1];
+    const winNumbers = shuffle.slice(0, 6).sort((previous, current) => previous - current);
+    return [...winNumbers, bonusNumber]
+  }
   export default {
     components: {
       LottoBall
     },
     data() {
       return {
-        winBalls: [
-          {number : 5}
-        ],
+        winNumbers: getWinNumbers(),
+        winBalls: [],
         bonus: null,
         redo: false
       }
     },
     mounted() {
-      
+      console.log(this.winNumbers)
     },
     beforeDestory() {
 
