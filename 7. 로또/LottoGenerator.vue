9396dc05124f64785173ba4802abdbf932ef039a
webdevyoo <yjk7454@naver.com>
2024-11-21 21:13:03 +0900

    feat: `7. LottoGenerator.vue` - 로또 생성 컴포넌트 레아이웃 구현

--- 7. 로또/LottoGenerator.vue
+++ 7. 로또/LottoGenerator.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    
+    <div>당첨 숫자</div>
+    <div id="결과창">
+    </div>
+    <div>보너스</div>
+    <button v-if="redo">한 번 더!</button>
   </div>
 </template>
 <script>
@@ -8,6 +12,7 @@
   export default {
     data() {
       return {
+        redo: false
       }
     },
     mounted() {
