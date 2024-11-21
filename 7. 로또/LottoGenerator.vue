f46c0961c531bb557619162e8ad2cb13ff5a7d10
webdevyoo <yjk7454@naver.com>
2024-11-21 21:25:02 +0900

    feat: `7. LottoGenerator.vue` - 로또 생성 컴포넌트 로또 공 컴포넌트 import및 템플릿 적용

--- 7. 로또/LottoGenerator.vue
+++ 7. 로또/LottoGenerator.vue
@@ -2,16 +2,25 @@
   <div>
     <div>당첨 숫자</div>
     <div id="결과창">
+      <LottoBall v-for="(ball, idx) in winBalls" :number="ball.number" :key="idx"/>
     </div>
     <div>보너스</div>
+    <LottoBall v-if="bonus" :number="3"/>
     <button v-if="redo">한 번 더!</button>
   </div>
 </template>
 <script>
-  
+  import LottoBall from './LottoBall'
   export default {
+    components: {
+      LottoBall
+    },
     data() {
       return {
+        winBalls: [
+          {number : 5}
+        ],
+        bonus: null,
         redo: false
       }
     },
