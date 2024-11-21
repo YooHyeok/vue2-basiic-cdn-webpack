d689efc06a7b0759f8283da0d15863b07581ff33
webdevyoo <yjk7454@naver.com>
2024-11-21 21:08:50 +0900

    feat: `7. 로또` - 프로젝트 초기 세팅

---
+++ 7. 로또/LottoGenerator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+  
+  export default {
+    data() {
+      return {
+      }
+    },
+    mounted() {
+      
+    },
+    beforeDestory() {
+
+    },
+    computed: {
+      
+    },
+    watch: {
+
+    },
+    methods: {
+      
+    },
+  }
+</script>
+<style scoped>
+</style>
---
+++ 7. 로또/main.js
@@ -0,0 +1,6 @@
+// import Vue from 'vue'
+import { createApp } from 'vue';
+import LottoGenerator from './LottoGenerator'
+
+// new Vue(RockScissorsPapper).$mount('#root'); // vue2 마운트
+createApp(LottoGenerator).mount('#root'); // vue3 마운트
