7e949107462196bb25fa6911e488887ae40e70a6
webdevyoo <yjk7454@naver.com>
2024-11-23 21:21:35 +0900

    feat: `8. 틱택토` - 초기 프로젝트 세팅 및 기본 컴포넌트 구성

---
+++ 8. 틱택토/main.js
@@ -0,0 +1,6 @@
+// import Vue from 'vue'
+import { createApp } from 'vue';
+import TicTacToe from './TicTacToe'
+
+// new Vue(RockScissorsPapper).$mount('#root'); // vue2 마운트
+createApp(TicTacToe).mount('#root'); // vue3 마운트
