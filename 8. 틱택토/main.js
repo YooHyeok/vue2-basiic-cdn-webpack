4a4bb87e76e5acdd0ef963199259deb14c080432
webdevyoo <yjk7454@naver.com>
2024-11-26 01:12:26 +0900

    style: `main.js` - 컴포넌트명 이탈자 수정

--- 8. 틱택토/main.js
+++ 8. 틱택토/main.js
@@ -2,5 +2,5 @@
 import { createApp } from 'vue';
 import TicTacToe from './TicTacToe'
 
-// new Vue(RockScissorsPapper).$mount('#root'); // vue2 마운트
+// new Vue(TicTacToe).$mount('#root'); // vue2 마운트
 createApp(TicTacToe).mount('#root'); // vue3 마운트
