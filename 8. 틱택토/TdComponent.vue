54ef1a00ca34ceb93ddbd1181c05ab29bd8f9dcb
webdevyoo <yjk7454@naver.com>
2024-11-26 01:11:07 +0900

    feat: `8. TdComponent.vue` - 3목 여부 확인 및 승|무 결과 출력 및 초기화 로직 구현

--- 8. 틱택토/TdComponent.vue
+++ 8. 틱택토/TdComponent.vue
@@ -15,14 +15,51 @@ export default {
       console.log(this.$root.$data) // 가장 최상위 부모의 data변수 접근
       this.$root.example() // 가장 최상위 부모 메소드 접근
       console.log(this.$parent.$data) // 직계 부모의 data변수 접근
-      const rootData = this.$root.$data;
       this.$parent.example()
+      
+      const rootData = this.$root.$data;
       rootData.tableData
         [this.rowIndex] /* 클릭 된 가로(tr) 행 */
         [this.cellIndex] /* 클릭 된 세로(td) 행 */
           = rootData.turn; /* 현재 turn(팀) 값으로 초기화 */
       // this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn) // [arg1]: 바꿀 대상 [arg2]: key|index [arg3]: 바꿀 값 - Vue3에서는 더이상 지원하지 않음
-      rootData.turn = rootData.turn === 'O' ? 'X' : 'O'
+      /* 가로/세로/대각 3목 여부 */
+      let win = false;
+      if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+        win = true; // 가로줄 3목 여부
+      }
+      if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+        win = true; // 세로줄 3목 여부
+      }
+      if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+        win = true; // 대각선↘ 3목 여부
+      }
+      if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+        win = true; // 대각선↙ 3목 여부
+      }
+      /* 이긴 경우(3목 달성) */
+      if (win) {
+        rootData.winner = rootData.turn;
+        rootData.turn = 'O';
+        rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']]
+      } else { /* 무승부 */
+        let all = true;
+        rootData.tableData.forEach((row) => { /* 무승부 검사 */
+          row.forEach((cell) => {
+            console.log("cell : ", cell, " !cell : ", !cell)
+            if (!cell) { /* ''일 경우 */
+              all = false;
+            }
+          });
+        });
+        if (all) { /* 무승부 */
+          rootData.winner = '';
+          rootData.turn = 'O';
+          rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']]
+        } else {
+          rootData.turn = rootData.turn === 'O' ? 'X' : 'O'
+        }
+      }
     }
   }
 }
