271a97d2795697c8c323b045018dc21723326c32
webdevyoo <yjk7454@naver.com>
2024-11-28 21:31:48 +0900

    refactor: `8. TdComponent.vue` - 반복 사용 변수 rowIndex, cellIndex const 추출

--- 8. 틱택토/TdComponent.vue
+++ 8. 틱택토/TdComponent.vue
@@ -16,20 +16,23 @@ export default {
       this.$root.example() // 가장 최상위 부모 메소드 접근
       console.log(this.$parent.$data) // 직계 부모의 data변수 접근
       this.$parent.example()
-      
       if (this.cellData) return; // 선택된 cell 재클릭 방지
+
+      const cellIndex = this.cellIndex;
+      const rowIndex = this.rowIndex;
       const rootData = this.$root.$data;
+      
       rootData.tableData
-        [this.rowIndex] /* 클릭 된 가로(tr) 행 */
-        [this.cellIndex] /* 클릭 된 세로(td) 행 */
+        [rowIndex] /* 클릭 된 가로(tr) 행 */
+        [cellIndex] /* 클릭 된 세로(td) 행 */
           = rootData.turn; /* 현재 turn(팀) 값으로 초기화 */
-      // this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn) // [arg1]: 바꿀 대상 [arg2]: key|index [arg3]: 바꿀 값 - Vue3에서는 더이상 지원하지 않음
+      // this.$set(rootData.tableData[rowIndex], cellIndex, rootData.turn) // [arg1]: 바꿀 대상 [arg2]: key|index [arg3]: 바꿀 값 - Vue3에서는 더이상 지원하지 않음
       /* 가로/세로/대각 3목 여부 */
       let win = false;
-      if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+      if (rootData.tableData[rowIndex][0] === rootData.turn && rootData.tableData[rowIndex][1] === rootData.turn && rootData.tableData[rowIndex][2] === rootData.turn) {
         win = true; // 가로줄 3목 여부
       }
-      if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+      if (rootData.tableData[0][cellIndex] === rootData.turn && rootData.tableData[1][cellIndex] === rootData.turn && rootData.tableData[2][cellIndex] === rootData.turn) {
         win = true; // 세로줄 3목 여부
       }
       if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
