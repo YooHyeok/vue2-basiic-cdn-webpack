fe643b02bb0b3692f7e8ac15ac2aa705e4988544
webdevyoo <yjk7454@naver.com>
2024-12-01 01:45:01 +0900

    feat: `8. Vuex` - TdComponent state(computed), mutations(commit) 적용

--- 8. 틱택토 Vuex/TdComponent.vue
+++ 8. 틱택토 Vuex/TdComponent.vue
@@ -5,10 +5,19 @@
 export default {
   name: 'TdComponent',
   props: {
-    rowData: Array,
     rowIndex: Number,
-    cellData: String,
     cellIndex: Number,
+  },
+  computed: {
+    tableData() {
+      return this.$store.state.tableData;
+    },
+    cellData() {
+      return this.$store.state.tableData[this.rowIndex][this.cellIndex];
+    },
+    turn() {
+      return this.$store.state.turn;
+    },
   },
   methods: {
     onClickTd() {
@@ -16,48 +25,42 @@ export default {
 
       const cellIndex = this.cellIndex;
       const rowIndex = this.rowIndex;
-      const rootData = this.$root.$data;
+      const tableData = this.tableData;
+      const turn = this.turn;
       
-      rootData.tableData
-        [rowIndex] /* 클릭 된 가로(tr) 행 */
-        [cellIndex] /* 클릭 된 세로(td) 행 */
-          = rootData.turn; /* 현재 turn(팀) 값으로 초기화 */
-      // this.$set(rootData.tableData[rowIndex], cellIndex, rootData.turn) // [arg1]: 바꿀 대상 [arg2]: key|index [arg3]: 바꿀 값 - Vue3에서는 더이상 지원하지 않음
-      /* 가로/세로/대각 3목 여부 */
-      let win = false;
-      if (rootData.tableData[rowIndex][0] === rootData.turn && rootData.tableData[rowIndex][1] === rootData.turn && rootData.tableData[rowIndex][2] === rootData.turn) {
+      this.$store.commit('CLICK_CELL', {row: rowIndex, cell: cellIndex}) // CLICK_CELL mutations 메소드 호출(commit)
+      let win = false; // 승/무 여부
+      /* 3목 여부 확인 */
+      if (tableData[rowIndex][0] === turn && tableData[rowIndex][1] === turn && tableData[rowIndex][2] === turn) {
         win = true; // 가로줄 3목 여부
       }
-      if (rootData.tableData[0][cellIndex] === rootData.turn && rootData.tableData[1][cellIndex] === rootData.turn && rootData.tableData[2][cellIndex] === rootData.turn) {
+      if (tableData[0][cellIndex] === turn && tableData[1][cellIndex] === turn && tableData[2][cellIndex] === turn) {
         win = true; // 세로줄 3목 여부
       }
-      if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+      if (tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn) {
         win = true; // 대각선↘ 3목 여부
       }
-      if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+      if (tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn) {
         win = true; // 대각선↙ 3목 여부
       }
       /* 이긴 경우(3목 달성) */
       if (win) {
-        rootData.winner = rootData.turn;
-        rootData.turn = 'O';
-        rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']]
+        this.$store.commit('SET_WINNER', turn) // SET_WINNER mutations 메소드 호출(commit)
+        this.$store.commit('RESET_GAME') // RESET_GAME mutations 메소드 호출(commit)
       } else { /* 무승부 */
         let all = true;
-        rootData.tableData.forEach((row) => { /* 무승부 검사 */
+        tableData.forEach((row) => { /* 무승부 검사 */
           row.forEach((cell) => {
-            console.log("cell : ", cell, " !cell : ", !cell)
             if (!cell) { /* ''일 경우 */
               all = false;
             }
           });
         });
         if (all) { /* 무승부 */
-          rootData.winner = '';
-          rootData.turn = 'O';
-          rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']]
-        } else {
-          rootData.turn = rootData.turn === 'O' ? 'X' : 'O'
+          this.$store.commit('NO_WINNER') // NO_WINNER mutations 메소드 호출(commit)
+          this.$store.commit('RESET_GAME') // RESET_GAME mutations 메소드 호출(commit)
+        } else { // !(승&&무)
+          this.$store.commit('CHANGE_TURN') // CHANGE_TURN mutations 메소드 호출(commit)
         }
       }
     }
