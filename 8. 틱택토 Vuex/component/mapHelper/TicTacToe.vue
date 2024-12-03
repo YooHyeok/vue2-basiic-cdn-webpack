b8d4da175daeafb6d3300a495fc7a0afe2b761d5
webdevyoo <yjk7454@naver.com>
2024-12-04 03:16:01 +0900

    feat: `8. Vuex` - TicTacToe mapHelper(state,getters) 적용

--- 8. 틱택토 Vuex/component/mapHelper/TicTacToe.vue
+++ 8. 틱택토 Vuex/component/mapHelper/TicTacToe.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <div>{{ turnMessage }}</div>
+    <div>{{ turnMsg }}</div>
     <TableCompnent/>
-    <div v-if="winner">{{ winMessage }}</div>
+    <div v-if="winner">{{ winMsg }}</div>
   </div>
 </template>
 <script>
 import TableCompnent from './TableCompnent.vue';
+import { mapState, mapGetters } from 'vuex';
 export default {
   components: {
       TableCompnent
   },
   computed: {
-    store() {
-      return this.$store
-    },
-    turnMessage() {
-      return this.store.getters.turnMessage
-    },
-    winner() {
-      return this.store.state.winner
-    },
-    winMessage() {
-      return this.store.getters.winMessage
-    }
+    ...mapState(['winner', 'turn']),
+    ...mapState({
+      winnerV2: state=> state.winner,
+      turnV2: state=> state.turn
+    }),
+    ...mapState({
+      winnerV3(state) {state.winner + 1}, 
+      turnV3(state) {state.turn + this.value} // 기존 state에 컴포넌트 data 조합(일반함수는 this 사용 가능)
+    }),
+    ...mapGetters(['winMessage']),
+    ...mapGetters(['turnMessage']),
+    ...mapGetters({winMsg: 'winMessage'}), // getters는 화살표 함수 참조가 불가능하다.
+    ...mapGetters({turnMsg: 'turnMessage'}), // getters는 화살표 함수 참조가 불가능하다.
   },
 }
 </script>
