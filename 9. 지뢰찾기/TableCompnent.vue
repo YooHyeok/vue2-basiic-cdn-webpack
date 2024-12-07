bb9316ce7cf2e35bc50acba0da313c800dae6380
webdevyoo <yjk7454@naver.com>
2024-12-08 00:23:44 +0900

    feat: `9. 지뢰찾기` - TableComponent 각 칸 코드별 색상 적용 computed 구현

--- 9. 지뢰찾기/TableCompnent.vue
+++ 9. 지뢰찾기/TableCompnent.vue
@@ -1,18 +1,71 @@
 <template>
   <table>
     <tr v-for="(rowData, rowIndex) in tableData" :key="rowIndex">
-      <td v-for="(cellData, cellIndex) in rowData" :key="cellIndex">
-        {{ cellData }}
+      <td v-for="(cellData, cellIndex) in rowData" :key="cellIndex" :style="cellDataStyle(rowIndex, cellIndex)">
+        {{ cellDataText(rowIndex, cellIndex) }}
       </td>
     </tr>
   </table>
 </template>
 <script>
 import { mapState } from "vuex";
+import store, { CODE } from "./store";
 export default {
+  store,
   name: 'TableComponent',
+  data() {
+    return {
+      value: "바보"
+    }
+  },
   computed: {
-    ...mapState(['tableData'])
+    ...mapState(['tableData']),
+    cellDataStyle(state) {
+       return function(row, cell) {
+        switch (store.state.tableData[row][cell]) {
+          case CODE.NORMAL : 
+          case CODE.MINE : 
+            return {
+              background: '#444'
+            };
+          case CODE.CLICKED_MINE : 
+          case CODE.OPENED : 
+            return {
+              background: 'white'
+            };
+          case CODE.FLAG : 
+          case CODE.FLAG_MINE : 
+            return {
+              background: 'red'
+            };
+          case CODE.QUESTION : 
+          case CODE.QUSTION_MINE : 
+            return {
+              background: 'yellow'
+            };
+          default : 
+            return {};
+        }
+      }
+    },
+    cellDataText() {
+      return function(row, cell) {
+        switch (store.state.tableData[row][cell]) {
+          case CODE.NORMAL :
+            return '';
+          case CODE.MINE :
+            return 'X';
+          case CODE.FLAG, CODE.FLAG_MINE :
+            return '!'
+          case CODE.QUESTION, CODE.QUSTION_MINE :
+            return '?';
+          case CODE.CLICKED_MINE :
+            return '펑';
+          default : 
+            return '';
+        }
+      }
+    },
   }
 }
 </script>
