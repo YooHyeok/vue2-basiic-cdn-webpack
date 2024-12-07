78f6a6feb9fffa7acc6c37d5f90ba30c529ac82b
webdevyoo <yjk7454@naver.com>
2024-12-07 18:51:51 +0900

    feat: `9. 지뢰찾기` - MineSweeper.vue  state 및 MineForm, TableComponent 적용

--- 9. 지뢰찾기/MineSweeper.vue
+++ 9. 지뢰찾기/MineSweeper.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
+    <MineForm/>
+    <div>{{ timer }}</div>
+    <TableCompnent/>
+    <div>{{ result }}</div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+import store from './store';
+import TableCompnent from './TableCompnent.vue';
+import MineForm from './MineForm.vue';
 export default {
+  store,
   components: {
+    MineForm,
+    TableCompnent
   },
   computed: {
+   ...mapState(['timer', 'result'])
   },
   methods: {
    
   }
 }
 </script>
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
