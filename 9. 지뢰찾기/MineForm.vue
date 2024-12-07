45cfb271731952c43a07ec66febecbf9fe3faa71
webdevyoo <yjk7454@naver.com>
2024-12-07 18:30:07 +0900

    feat: `9. 지뢰찾기` - MineForm.vue 입력폼 및 이벤트 , 버튼 START_GAME mutations 호출 구현

---
+++ 9. 지뢰찾기/MineForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="number" placeholder="세로" :value="row" @change="onChangeRow">
+    <input type="number" placeholder="가로" :value="cell" @change="onChangeCell">
+    <input type="number" placeholder="지뢰" :value="mine" @change="onChangeMine">
+    <button @click="onClickBtn">시작</button>
+  </div>
+</template>
+<script>
+import store, { START_GAME } from './store'
+export default {
+  name: 'MineForm',
+  data() {
+    return {
+      row: 10,
+      cell: 10,
+      mine: 10,
+    }
+  },
+  methods: {
+    onChangeRow(e) {
+      this.row = e.target.value;
+    },
+    onChangeCell(e) {
+      this.cell = e.target.value;
+    },
+    onChangeMine(e) {
+      this.mine = e.target.value;
+    },
+    /**
+     * ## 버튼 클릭 함수
+     * START_GAME mutations을 호출하며 입력된 데이터 전송
+     */
+    onClickBtn() {
+      store.commit(START_GAME, { row: this.row, cell: this.cell, mine: this.mine })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
