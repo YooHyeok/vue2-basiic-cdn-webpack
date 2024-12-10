b60f5ef61e5fae37514c926eb90fb6a48c4d7710
webdevyoo <yjk7454@naver.com>
2024-12-11 00:22:49 +0900

    feat: `VueRouter` - VueRouter 프로젝트 초기 세팅

---
+++ vue-router/Router.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Router'
+}
+</script>
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
