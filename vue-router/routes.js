688f630f8f436d382609e270b47d1b02afb1a1af
webdevyoo <yjk7454@naver.com>
2024-12-11 01:14:27 +0900

    feat: `VueRouter` - 라우터 모듈 routes 속성 틱택토 컴포넌트 등록 및 path 매핑

--- vue-router/routes.js
+++ vue-router/routes.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import TicTacToe from '../8. 틱택토 EventBus/TicTacToe.vue'
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/tictactoe', component: TicTacToe },
+  ],
 });
